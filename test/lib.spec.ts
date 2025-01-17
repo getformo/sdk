6e2a43f763e7cbd389daeadc85f5ca24395506a8
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2025-01-17 13:04:17 +0700

    [P-895] toSnakeCase Exceeding max call stack

---
+++ test/lib.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it } from "mocha";
+import { toSnakeCase } from "../src/lib/utils";
+import { expect } from "chai";
+
+describe("toSnakeCase", () => {
+  it("should convert object keys to snake case", () => {
+    expect(
+      toSnakeCase({ chainId: "12345", hashMessage: "John Doe" })
+    ).to.deep.equal({
+      chain_id: "12345",
+      hash_message: "John Doe",
+    });
+  });
+
+  it("should convert object keys to snake case, omitting keys in the omitKeys array", () => {
+    expect(
+      toSnakeCase(
+        {
+          chainId: "12345",
+          hashMessage: "John Doe",
+          "user-agent": "Mozilla/5.0",
+        },
+        ["user-agent"]
+      )
+    ).to.deep.equal({
+      chain_id: "12345",
+      hash_message: "John Doe",
+      "user-agent": "Mozilla/5.0",
+    });
+  });
+});
