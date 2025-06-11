9599400b09bf9c44c60e49a1ec609478fb926b9a
Huỳnh Thanh Tâm <55596752+TamHuynhTee@users.noreply.github.com>
2025-06-11 13:44:45 +0700

    add script for initialization

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export * from './FormoAnalyticsProvider';
-export * from './FormoAnalytics';
-export * from './types';
+import { FormoAnalytics } from "./FormoAnalytics";
+
+export * from "./FormoAnalyticsProvider";
+export * from "./FormoAnalytics";
+export * from "./types";
+
+export function formofy(writeKey: string) {
+  if (writeKey && typeof window !== "undefined") {
+    FormoAnalytics.init(writeKey)
+      .then((f) => {
+        (window as any).formo = f;
+      })
+      .catch((e) => console.error("Error initializing FormoAnalytics:", e));
+  } else {
+    console.warn("FormoAnalytics not found");
+  }
+}
