eebdf7280e5fa964b29b9af4ca8a3f8f1bae93ab
Yos Riady <yos@formo.so>
2025-08-28 09:46:55 +0700

    fix: Fix duplicate identify events in a session

--- src/constants/base.ts
+++ src/constants/base.ts
@@ -1,5 +1,6 @@
 export const SESSION_TRAFFIC_SOURCE_KEY = "traffic-source";
 export const SESSION_WALLET_DETECTED_KEY = "wallet-detected";
+export const SESSION_WALLET_IDENTIFIED_KEY = "wallet-identified";
 export const SESSION_CURRENT_URL_KEY = "analytics-current-url";
 export const SESSION_USER_ID_KEY = "user-id";
 
