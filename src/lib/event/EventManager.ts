404b509bf60a2cfec38cdf490bca23843211e248
Yos Riady <yos@formo.so>
2025-09-08 14:05:35 +0700

    Block dummy addresses

--- src/lib/event/EventManager.ts
+++ src/lib/event/EventManager.ts
@@ -3,6 +3,7 @@ import { logger } from "../logger";
 import { IEventQueue } from "../queue";
 import { EventFactory } from "./EventFactory";
 import { IEventFactory, IEventManager } from "./type";
+import { isBlockedAddress } from "../../utils/address";
 
 /**
  * A service to generate valid event payloads and queue them for processing
@@ -27,6 +28,13 @@ class EventManager implements IEventManager {
   addEvent(event: APIEvent, address?: Address, userId?: string): void {
     const { callback, ..._event } = event;
     const formoEvent = this.eventFactory.create(_event, address, userId);
+    
+    // Check if the final event has a blocked address - don't queue it
+    if (formoEvent.address && isBlockedAddress(formoEvent.address)) {
+      logger.warn(`Event blocked: Address ${formoEvent.address} is in the blocked list and cannot emit events`);
+      return;
+    }
+
     this.eventQueue.enqueue(formoEvent, (err, _, data) => {
       if (err) {
         logger.error("Error sending events:", err);
